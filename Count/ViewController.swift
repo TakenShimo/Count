c04c013d657017812db534f16735bac1f0468f97
TakenShimo <shimojimatakeshi@macbook-pro-5.local>
2018-10-19 19:39:54 +0900

    Initial Commit

---
+++ Count/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Count
+//
+//  Created by shimojimatakeshi on 2018/10/19.
+//  Copyright © 2018年 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
